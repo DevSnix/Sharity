dfa8182e433111f70abf395b5495d60432944c8d
DevSnix <xnixhere@gmail.com>
2024-07-21 08:04:24 +0300

    *Moved the class functions below the getters and setters as common practice for the industry +Added comment for further clarification on userStatus variable

--- app/src/main/java/com/test/sharity/User.java
+++ app/src/main/java/com/test/sharity/User.java
@@ -20,6 +20,7 @@ public class User {
     private String userEmail;
     private String userPhoneNumber;
     private String userAddress;
+    // if userStatus = false -> user is not active, otherwise it is active (true)
     private boolean userStatus;
 
     public User() {
@@ -27,79 +28,8 @@ public class User {
     }
 
     public User(String userName, String userPassword, String userType, String userEmail, String userPhoneNumber, String userAddress) {
-        register(userName, userPassword, userType, userEmail, userPhoneNumber, userAddress);
+        registerUser(userName, userPassword, userType, userEmail, userPhoneNumber, userAddress);
     }
-
-    public void register(String userName, String userPassword, String userType, String userEmail, String userPhoneNumber, String userAddress) {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference usersRef = database.getReference("users");
-        Random rand = new Random();
-
-        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                int newUserId;
-                do {
-                    newUserId = rand.nextInt(100000);  // Generate a random user ID
-                } while (snapshot.hasChild(String.valueOf(newUserId)));  // Check if the ID already exists
-
-                userId = newUserId;
-                User.this.userName = userName;
-                User.this.userPassword = userPassword;
-                User.this.userType = userType;
-                User.this.userEmail = userEmail;
-                User.this.userPhoneNumber = userPhoneNumber;
-                User.this.userAddress = userAddress;
-                userStatus = true;
-                saveToFirebase();
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-
-            }
-        });
-    }
-
-    public void saveToFirebase() {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference usersRef = database.getReference("users");
-        usersRef.child(String.valueOf(this.userId)).setValue(this);
-    }
-
-    public static void login(String userEmail, String userPassword, Activity activity) {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference usersRef = database.getReference("users");
-
-        usersRef.orderByChild("userEmail").equalTo(userEmail).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
-                        User user = userSnapshot.getValue(User.class);
-
-                        if (user != null && user.getUserPassword().equals(userPassword)) {
-                            Toast.makeText(activity, "Login successful", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(activity, MainActivity.class);
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            activity.startActivity(intent);
-                            activity.finish();
-                        } else {
-                            Toast.makeText(activity, "Incorrect password", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                } else {
-                    Toast.makeText(activity, "User not found", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Toast.makeText(activity, "Database error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
 
     public int getUserId() {
         return userId;
@@ -163,5 +93,77 @@ public class User {
 
     public void setUserStatus(boolean userStatus) {
         this.userStatus = userStatus;
+    }
+
+    public void registerUser(String userName, String userPassword, String userType, String userEmail, String userPhoneNumber, String userAddress) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference usersRef = database.getReference("users");
+        Random rand = new Random();
+
+        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                int newUserId;
+                do {
+                    newUserId = rand.nextInt(100000);  // Generate a random user ID
+                } while (snapshot.hasChild(String.valueOf(newUserId)));  // Check if the ID already exists
+
+                userId = newUserId;
+                User.this.userName = userName;
+                User.this.userPassword = userPassword;
+                User.this.userType = userType;
+                User.this.userEmail = userEmail;
+                User.this.userPhoneNumber = userPhoneNumber;
+                User.this.userAddress = userAddress;
+                userStatus = true;
+                saveToFirebase();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
+    }
+
+    //Save user to Firebase Realtime Database
+    public void saveToFirebase() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference usersRef = database.getReference("users");
+        usersRef.child(String.valueOf(this.userId)).setValue(this);
+    }
+
+    //Login user to application, checks if user email exists in the process
+    public static void login(String userEmail, String userPassword, Activity activity) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference usersRef = database.getReference("users");
+
+        usersRef.orderByChild("userEmail").equalTo(userEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                        User user = userSnapshot.getValue(User.class);
+
+                        if (user != null && user.getUserPassword().equals(userPassword)) {
+                            Toast.makeText(activity, "Login successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(activity, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            activity.startActivity(intent);
+                            activity.finish();
+                        } else {
+                            Toast.makeText(activity, "Incorrect password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                } else {
+                    Toast.makeText(activity, "User not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(activity, "Database error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
