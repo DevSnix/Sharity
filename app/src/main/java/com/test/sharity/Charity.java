bfbb917fd5e3846f44db4c57405ad928f2fef710
DevSnix <xnixhere@gmail.com>
2024-07-21 07:44:24 +0300

    Added: +Imported database libraries +registerCharity() +saveToFirebase() +sendRegisterRequestToAdmin() -> Empty function, will be implemented in the future Removed: -Commented out arrays because firebase doesn't support saving arrays. In the future the arrays will be changed to lists which is what firebase supports.

--- app/src/main/java/com/test/sharity/Charity.java
+++ app/src/main/java/com/test/sharity/Charity.java
@@ -1,5 +1,13 @@
 package com.test.sharity;
 
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Charity {
  private int licenseNumber;
  private String charityName;
@@ -11,31 +19,16 @@ public class Charity {
  private String imgUrl;
  private String charityDescription;
  private float rating;
+ /*
  private Donor charityDonors[];
  private Donation charityDonations[];
+
+  */
+ //if charityStatus = false -> charity account is not active, otherwise it is active (true)
  private boolean charityStatus;
 
- public Charity(int licenseNumber, String charityName, String charityEmail, String branchAddress, String charityPassword, String charityPhoneNumber, String charityType, String imgUrl
- , String charityDescription) {
-     this.licenseNumber = licenseNumber;
-     this.charityName = charityName;
-     this.charityEmail = charityEmail;
-     this.branchAddress = branchAddress;
-     this.charityPassword = charityPassword;
-     this.charityPhoneNumber = charityPhoneNumber;
-     this.charityType = charityType;
-     this.imgUrl = imgUrl;
-     this.charityDescription = charityDescription;
-     this.rating = 0;
-     this.charityDonors = new Donor[0];
-     this.charityDonations = new Donation[0];
-     this.charityStatus = false;
- }
-    public Charity(String name, String description, String imageUrl, float rating) {
-        this.charityName = name;
-        this.charityDescription = description;
-        this.imgUrl = imageUrl;
-        this.rating = rating;
+    public Charity(int licenseNumber, String charityName, String charityEmail, String branchAddress, String charityPassword, String charityPhoneNumber, String charityType, String imgUrl, String charityDescription) {
+        registerCharity(licenseNumber, charityName, charityEmail, branchAddress, charityPassword, charityPhoneNumber, charityType, imgUrl, charityDescription);
     }
 
     public Charity() {
@@ -122,6 +115,7 @@ public class Charity {
         this.rating = rating;
     }
 
+    /*
     public Donor[] getCharityDonors() {
         return charityDonors;
     }
@@ -137,6 +131,7 @@ public class Charity {
     public void setCharityDonations(Donation[] charityDonations) {
         this.charityDonations = charityDonations;
     }
+     */
 
     public boolean isCharityStatus() {
         return charityStatus;
@@ -144,5 +139,54 @@ public class Charity {
 
     public void setCharityStatus(boolean charityStatus) {
         this.charityStatus = charityStatus;
+    }
+
+    //Register charity to Firebase Realtime Database
+    public void registerCharity(int licenseNumber, String charityName, String charityEmail, String branchAddress, String charityPassword, String charityPhoneNumber, String charityType, String imgUrl, String charityDescription) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference charitiesRef = database.getReference("charities");
+
+        charitiesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if (snapshot.hasChild(String.valueOf(licenseNumber))) {
+                    // Charity with this license number already exists
+                    return;
+                } else {
+                    Charity.this.licenseNumber = licenseNumber;
+                    Charity.this.charityName = charityName;
+                    Charity.this.charityEmail = charityEmail;
+                    Charity.this.branchAddress = branchAddress;
+                    Charity.this.charityPassword = charityPassword;
+                    Charity.this.charityPhoneNumber = charityPhoneNumber;
+                    Charity.this.charityType = charityType;
+                    Charity.this.imgUrl = imgUrl;
+                    Charity.this.charityDescription = charityDescription;
+                    Charity.this.rating = 0;
+                    /*
+                    Charity.this.charityDonors = new Donor[0];
+                    Charity.this.charityDonations = new Donation[0];
+
+                     */
+                    Charity.this.charityStatus = false;
+                    saveToFirebase();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Handle possible errors
+            }
+        });
+    }
+
+    private void saveToFirebase() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference charitiesRef = database.getReference("charities");
+        charitiesRef.child(String.valueOf(this.licenseNumber)).setValue(this);
+    }
+
+    public void sendRegisterRequestToAdmin() {
+
     }
 }
