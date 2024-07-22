475690be12b87fe6a734fb77a4bdb457dc211243
DevSnix <xnixhere@gmail.com>
2024-07-22 12:11:37 +0300

    Changed: *Arrays to Lists due to arrays not being compatible with Firebase. Added: +Getters & Setters

--- app/src/main/java/com/test/sharity/Donor.java
+++ app/src/main/java/com/test/sharity/Donor.java
@@ -1,6 +1,36 @@
 package com.test.sharity;
 
+import java.util.List;
+
 public class Donor extends User {
-    private Donation userDonations[];
-    private Charity followedCharities[];
+    private List<Donation> userDonations;
+    private List<Charity> myCharities;
+
+    // Default constructor for Firebase
+    public Donor() {
+    }
+
+    // Constructor
+    public Donor(String userName, String userPassword, String userType, String userEmail, String userPhoneNumber, String userAddress, List<Donation> userDonations, List<Charity> myCharities) {
+        super(userName, userPassword, userType, userEmail, userPhoneNumber, userAddress);
+        this.userDonations = userDonations;
+        this.myCharities = myCharities;
+    }
+
+    // Getters and setters
+    public List<Donation> getUserDonations() {
+        return userDonations;
+    }
+
+    public void setUserDonations(List<Donation> userDonations) {
+        this.userDonations = userDonations;
+    }
+
+    public List<Charity> getMyCharities() {
+        return myCharities;
+    }
+
+    public void setMyCharities(List<Charity> myCharities) {
+        this.myCharities = myCharities;
+    }
 }
