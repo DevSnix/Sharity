50bfb6ab58070fa6bc26c7870231aa430308ffcb
DevSnix <xnixhere@gmail.com>
2024-08-27 07:59:57 +0300

    Added a statement which allows synchronization between it and firebase when the application losses its connection, to when it is restored.

--- app/src/main/java/com/test/sharity/SharityApplication.java
+++ app/src/main/java/com/test/sharity/SharityApplication.java
@@ -14,6 +14,5 @@ public class SharityApplication extends Application {
 
         //If the user is offline, the data changes will be queued and automatically synchronized when the user goes online.
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-
     }
 }
