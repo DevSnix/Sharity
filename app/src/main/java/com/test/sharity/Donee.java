e9d5a1566a7e297cc60767abd51a11c23423935c
DevSnix <xnixhere@gmail.com>
2024-08-27 07:46:07 +0300

    Added Donee logic which now makes a new Donee object instead of a User when a new user is made with the type of Donee.

--- app/src/main/java/com/test/sharity/Donee.java
+++ app/src/main/java/com/test/sharity/Donee.java
@@ -1,7 +1,45 @@
 package com.test.sharity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Donee extends User {
     private String donationRequestDescription;
     private String charityType;
-    private Charity followedCharities[];
+    private List<Charity> followedCharities;
+
+    // Default constructor for Firebase
+    public Donee() {
+        this.followedCharities = new ArrayList<>();
+    }
+
+    // Constructor
+    public Donee(String userName, String userPassword, String userType, String userEmail, String userPhoneNumber, String userAddress) {
+        super(userName, userPassword, userType, userEmail, userPhoneNumber, userAddress);
+        this.followedCharities = new ArrayList<>();
+    }
+
+    public List<Charity> getFollowedCharities() {
+        return followedCharities;
+    }
+
+    public void setFollowedCharities(List<Charity> followedCharities) {
+        this.followedCharities = followedCharities;
+    }
+
+    public String getCharityType() {
+        return charityType;
+    }
+
+    public void setCharityType(String charityType) {
+        this.charityType = charityType;
+    }
+
+    public String getDonationRequestDescription() {
+        return donationRequestDescription;
+    }
+
+    public void setDonationRequestDescription(String donationRequestDescription) {
+        this.donationRequestDescription = donationRequestDescription;
+    }
 }
